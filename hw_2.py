72ee5b679e884907b2e7b23af89d8bcacac856ef
m1zun0kami <117219886+m1zun0kami@users.noreply.github.com>
2026-09-18 14:47:24 +0300

    hw_2.py

--- hw_2.py
+++ hw_2.py
@@ -1,6 +1,7 @@
 import re
 from typing import Callable
 
+
 def generator_numbers(text: str):
     """
     Ітерує по всіх числах float відокремлених пробілами.
@@ -24,4 +25,4 @@ def sum_profit(text: str, func: Callable):
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
-print(f'Загальний дохід: {total_income}')
+print(f'Загальний дохід: {total_income}')  # Загальний дохід: 1351.46
